b5c17568596a7644e8cd958e87b83cf0ca8b3e54
Hera Miao <heram@andrew.cmu.edu>
2018-10-23 14:13:49 -0400

    added relationships and validations for conversion and transaction details model

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,2 +1,6 @@
 class Conversion < ApplicationRecord
+
+    # Relationships
+    belongs_to :split
+
 end
--- app/models/transaction_detail.rb
+++ app/models/transaction_detail.rb
@@ -1,2 +1,9 @@
 class TransactionDetail < ApplicationRecord
+
+    # Relationships
+    belongs_to :split
+
+    # Validations
+    validates :expense_type, inclusion: { in: %w[food transportation shopping entertainment lodging other] }
+
 end
