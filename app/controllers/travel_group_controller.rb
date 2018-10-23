97e184065542fefcac0cc320044accf1ea45b841
Hera Miao <heram@andrew.cmu.edu>
2018-10-23 13:57:33 -0400

    travel group controller created

---
+++ app/controllers/travel_group_controller.rb
@@ -0,0 +1,51 @@
+class TravelGroupController < ApplicationController
+
+    before_action :set_travel_group, only: [:show, :update, :destroy]
+  
+    # GET /travelgroup
+    def index
+      @travelgroups = TravelGroup.all
+      render json: @travelgroups
+    end
+  
+    # GET /travelgroup/1
+    def show
+      render json: @travelgroup
+    end
+  
+    # POST /travelgroup
+    def create
+      @travelgroup = TravelGroup.new(travel_group_params)
+  
+      if @travelgroup.save
+        render json: @travelgroup, status: :created, location: @travelgroup
+      else
+        render json: @travelgroup.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /travelgroup/1
+    def update
+      if @travelgroup.update(travel_group_params)
+        render json: @travelgroup
+      else
+        render json: @travelgroup.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /travelgroup/1
+    def destroy
+      @travelgroup.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_travel_group
+        @travel_group = TravelGroup.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def travel_group_params
+        params.permit(:trip_name, :start_date, :end_date)
+      end
+  end
--- app/models/travel_group.rb
+++ app/models/travel_group.rb
@@ -8,11 +8,11 @@ class TravelGroup < ApplicationRecord
     # Scopes
     scope :alphabetical, -> { order(:trip_name) }
     scope :chronological, -> { order(start_date: :desc ) }
-    scope :current_trip, ->(Date.current) { where("start_date <= ? AND (end_date > ? OR end_date IS NULL)", Date.today, Date.today) }
+    scope :current_trip, ->(Date.today) { where("start_date <= ? AND (end_date > ? OR end_date IS NULL)", Date.today, Date.today) }
 
     # Validations
     validates_presence_of :trip_name, :start_date
-    validates_date :start_date, on_or_before: lambda { Date.current }
+    validates_date :start_date, on_or_before: lambda { Date.today }
     validates_date :end_date, on_or_after: :start_date, allow_blank: true
 
 end
