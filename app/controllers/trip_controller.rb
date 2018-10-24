978ddc834da59107e62c224755ee11e1a095bc3f
Hera Miao <heram@andrew.cmu.edu>
2018-10-23 23:14:43 -0400

    trip swagger documentation

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,6 +1,20 @@
 class TripController < ApplicationController
     
-  
+    # Swagger documentation
+    swagger_controller :trip, "Trip"
+
+    swagger_api :index do
+        summary "Fetches all Trips"
+        notes "This lists all the trips"
+    end
+
+    swagger_api :show do
+        summary "Shows one trip"
+        param :path, :id, :integer, :required, "Trip ID"
+        notes "This details of one trip"
+        response :not_found
+    end
+
     before_action :set_trip, only: [:show]
   
     # GET /trip
