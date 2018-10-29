e35f07ac093d2fe531d52c4938744d65307021e3
Hera Miao <heram@andrew.cmu.edu>
2018-10-29 14:54:27 -0400

    added to new split and transaction models

--- app/models/split.rb
+++ app/models/split.rb
@@ -1,4 +1,13 @@
 class Split < ApplicationRecord
   belongs_to :transaction
   belongs_to :user
+
+  # Scopes
+  scope :paid, -> { where.not(date_paid: nil) }
+  scope :unpaid, -> { where(date_paid: nil) }
+
+  # Validations
+  validates_date :date_paid, on_or_after: :charge_date, allow_blank: true
+  validates :percent_owed, numericality { less_than_or_equal_to: 100, greater_than: 0}
+  # validate payee and payor are different people 
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,15 @@
 class Transaction < ApplicationRecord
+
+  # Relationships
   belongs_to :travel_group
   belongs_to :trip
   has_many :splits
+
+  # Scopes
+  scope :for_expense, ->(expense_type) { where(expense_type: expense_type) }
+  scope :for_country, -> (country) { where(country: country) }
+
+  # Validations
+  validates :expense_type, inclusion: { in: %w[food transportation shopping entertainment lodging other] }
+  validates :currency_type, inclusion: { in: %w[USD EUR GBP CHF AUD JPY TWD CNH] }
 end
