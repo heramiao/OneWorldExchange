6f965e45d357051ad4f99f22be03a7367be5ae17
Juliann Fields <julfieann@gmail.com>
2018-11-04 11:40:39 -0500

    added class to user, created blank group boards, started group class

---
+++ OWE Frontend/OWE Frontend/Group.swift
@@ -0,0 +1,15 @@
+//
+//  Group.swift
+//  OWE Frontend
+//
+//  Created by Juliann Fields on 11/4/18.
+//  Copyright © 2018 Juliann Fields. All rights reserved.
+//
+
+import Foundation
+
+class Group: {
+    var users: [User]()
+    var title: String
+    
+}
