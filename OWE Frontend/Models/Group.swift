b91ce1267425c092a9d657bf9c9ae2d36febd9aa
Juliann Fields <julfieann@gmail.com>
2018-11-06 11:08:34 -0500

    incremental changes after break

--- OWE Frontend/Models/Group.swift
+++ OWE Frontend/Models/Group.swift
@@ -12,11 +12,11 @@ import UIKit
 class Group: NSObject, NSCoding {
     
     // MARK: - Properties
-//    var users: [User]?
-    var title: String
+    var id: UUID = UUID()
+    var title: String = ""
     var image: UIImage?
-    var startDate: Date
-    var endDate: Date
+    var startDate: Date?
+    var endDate: Date?
     
     // MARK: - General
     override init() {
@@ -25,25 +25,16 @@ class Group: NSObject, NSCoding {
     
     // MARK: - Encoding
     required init(coder aDecoder: NSCoder) {
-//        self.users = aDecoder.decodeObject(forKey: "users") as? [User]
+        self.id = aDecoder.decodeObject(forKey: "id") as! UUID
         self.title = aDecoder.decodeObject(forKey: "title") as! String
         self.image = aDecoder.decodeObject(forKey: "image") as? UIImage
-        self.startDate = aDecoder.decodeObject(forKey: "start_date") as! Date
-        self.endDate = aDecoder.decodeObject(forKey: "end_date") as! Date
+        self.startDate = aDecoder.decodeObject(forKey: "start_date") as? Date
+        self.endDate = aDecoder.decodeObject(forKey: "end_date") as? Date
         super.init()
     }
     
-//    initialize 
-//    init(title: String, image: UIImage, users: [User], startDate: Date, endDate: Date) {
-//        self.users = users
-//        self.title = title
-//        self.startDate = startDate
-//        self.endDate = endDate
-//        self.image = image
-//    }
-    
     func encode(with aCoder: NSCoder) {
-//        aCoder.encode(users, forKey: "users")
+        aCoder.encode(id, forKey: "id")
         aCoder.encode(title, forKey: "title")
         aCoder.encode(image, forKey: "image")
         aCoder.encode(startDate, forKey: "start_date")
